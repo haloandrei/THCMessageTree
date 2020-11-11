a6a749c40adac5adaf3613a4f5ea19cfaf358223
haloandrei <haloandrei@gmail.com>
2020-11-11 10:59:44 +0200

    implemented React Flow Render, custom message Nodes, and an add system for messages

---
+++ src/components/message-node.js
@@ -0,0 +1,36 @@
+import React, { memo } from 'react';
+import { Handle } from 'react-flow-renderer';
+
+const messageNodeStyle = {
+    borderRadius: '7px',
+    border: '2px solid #94c7de',
+    background: '#ffffff',
+    color: '#000000',
+    padding: '0px 10px 10px 0px',
+};
+
+export default memo(({ data }) => {
+    return (
+        <div style={messageNodeStyle}>
+            <Handle
+                type="target"
+                position="top"
+                style={{ background: '#555' }}
+                onConnect={(params) => console.log('handle onConnect', params)}
+            />
+            <div style ={ {fontSize: '7px', top: '-10', left: '-10'}}>
+                <strong>{data.user.name}</strong>
+            </div>
+            <hr style={{marginTop: '-2px', marginRight:'-10px', border:'0px'}}/>
+            <div style={{fontSize: '12px',paddingLeft: '10px'}}>
+                {data.label}
+            </div>
+            <Handle
+                type="source"
+                position="bottom"
+                id="a"
+                style={{ background: '#555' }}
+            />
+        </div>
+    );
+});
---
+++ src/components/node-components.jsx
@@ -0,0 +1,105 @@
+import ReactFlow, {addEdge, Background, Controls, MiniMap, removeElements} from "react-flow-renderer";
+import React,{Fragment} from "react";
+import MessageNodeType from './message-node.js';
+
+
+export default class MessageNode extends React.Component{
+
+    state = {
+        messages : [
+            {id:'1', type: 'input', data:{label:'Group Chat'}, position: {x:0,y:0}},
+            // {id:'2', type: 'input', data:{label:'Hello'}, position: {x:12,y:12}}
+        ],
+        newMessageName : "This is where the new message is accumulated",
+        currentMessageId : '1'
+    }
+
+    onNodeDragStop = (event, node) => console.log('drag stop', node);
+
+    onElementClick = (event, element) => {this.setState({currentMessageId: element.id}); console.log('click', element);}
+
+    onElementsRemove = (elementsToRemove) =>{
+        this.setState({messages :  removeElements(elementsToRemove, this.state.messages)});
+    }
+
+
+    initBgColor = '#1A192B';
+
+    nodeTypes = {messageType: MessageNodeType,};
+
+    getNewId(){
+        return (this.state.messages.length + 1).toString();
+    }
+
+    onLoad(reactFlowInstance) {
+        reactFlowInstance.fitView();
+    }
+
+    addNode = () => {
+        let newId = this.getNewId();
+        this.setState({messages : this.state.messages.concat([{
+                id: newId,
+                type: 'messageType',
+                data: {label: this.state.newMessageName, user: {name: 'Onita Andrei'}},
+                position: {x:6,y:30} //{x: Math.random() * window.innerWidth, y: Math.random() * window.innerHeight}
+            },{
+                id: 'e'+this.state.currentMessageId + '-' + newId,
+                source: this.state.currentMessageId ,
+                target: newId,
+                animated: true,
+                style: { stroke: '#000000' },
+            }])});
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('mount', [this.state.messages, prevProps, prevState, snapshot]);
+    }
+
+    onConnect = (params) => this.setState({messages : addEdge(params,this.state.messages)});
+    //onConnect(params) { this.setState({messages : addEdge(params,this.state.messages)})} this creates it s own "this", use bind if you REALLY WANT TO HAVE FUNCTIONS
+
+    render() {
+        return(
+            <Fragment>
+            <ReactFlow
+                elements={this.state.messages}
+                onLoad={this.onLoad}
+                style = {{width: '100%', height: '90vh', background: this.bgColor}}
+                onConnect={this.onConnect}
+                connectionLineStyle={{stroke: "#ddd", strokeWidth: 3}}
+                connectionLineType="bezier"
+                nodeTypes={this.nodeTypes}
+                onElementClick={this.onElementClick}
+                onElementsRemove={this.onElementsRemove}
+                onNodeDragStop={this.onNodeDragStop}
+                // snapToGrid={true}
+                // snapGrid={[16,16]}
+                >
+                <Background
+                    color ="#888"
+                    gap={16}
+                />
+                <MiniMap
+                    nodeColor={n =>{
+                        if(n.type === 'input') return  'blue';
+                        return '#FFCC00';
+                    }}/>
+                <Controls/>
+            </ReactFlow>
+
+                <div>
+                    <input
+                        type = "text"
+                        name="title"
+                        onChange={e =>  this.setState({newMessageName: e.target.value})}
+                    />
+                    <button
+                        type="button"
+                        onClick={() => this.addNode()}
+                    >Add message</button>
+                </div>
+        </Fragment>
+        )
+        }
+
+}
